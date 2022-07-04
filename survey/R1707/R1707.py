dfe600c93b7e01b1134db69d96a4578a0fe2c48a
publicationdata <naelson.dc.oliveira@gmail.com>
2022-07-04 00:43:02 -0300

    repository cleanup

--- survey/R1707/R1707.py
+++
@@ -1,8 +0,0 @@
-def alocate_memory(self, memory=1024):
-    swap = 64
-    if memory < 512:
-        swap = 256,
-    diff = memory-swap
-    self.memory = memory
-    self.swap = swap
-    self.diff = diff
--- survey/R1710/R1710.py
+++
@@ -1,5 +0,0 @@
-def get_splited(text):
-    if 'a' in text:
-        return text.split('a')
-    elif 'b' in text:
-        return text.split('b')
